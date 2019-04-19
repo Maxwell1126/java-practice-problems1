bfea80a2fc791034be32bb63c76581259c8eb29c
Maxwell Todd <mtmaxtodd@gmail.com>
2019-04-19 13:54:01 -0500

    base commit

---
+++ src/main/java/com/practice/app/App.java
@@ -0,0 +1,19 @@
+package com.practice.app;
+
+// import static org.junit.Assert.assertEquals;
+// import org.junit.runners.JUnit4;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("x");
+
+
+    }
+    
+    
+}
---
+++ src/main/java/com/practice/app/CountingDuplicates.java
@@ -0,0 +1,18 @@
+package com.practice.app;
+import java.util.ArrayList;
+public class CountingDuplicates {
+    public static int duplicateCount(String text) {
+        ArrayList<String> dupes = new ArrayList<String>();
+        for (int i = 0; i < text.length(); i++) {
+            for (int j = i + 1; j < text.length(); j++) {
+                if ((text.charAt(i) == text.charAt(j)) && (!dupes.contains(Character.toString(text.charAt(i))))) {
+                    String s = Character.toString(text.charAt(i));
+                    dupes.add(s);
+                }
+            }
+        }
+
+        return dupes.size();
+    }
+
+}
